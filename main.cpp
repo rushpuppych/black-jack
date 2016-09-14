81aa25cd3d1590ea3a363d0d52ab8cba17942176
Andreas Dattilo <andreas.dattilo@hf-ict.info>
2016-09-14 09:20:01 +0200

    Created Card class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+#include <random>
 using namespace std;
 
+
+
+class Card(){
+
+    public:
+        int number;
+        char colour;
+    private:
+        int number_arr [] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        char colour_arr [] = {'red_heart', 'red_karo', 'black_pik', 'black_cross'};
+
+}
+
+
 int main() {
+
+    Card c;
+    c.set_values();
     cout << "Hello, World!" << endl;
     return 0;
 }
