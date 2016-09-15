ec7d1de2e060b3fa7fb016ecffba946b17a290af
Andreas Dattilo <andreas.dattilo@hf-ict.info>
2016-09-15 17:44:53 +0200

    Added gitignore

--- main.cpp
+++ main.cpp
@@ -5,21 +5,26 @@ using namespace std;
 
 
 class Card(){
-
-    public:
-        int number;
-        char colour;
     private:
         int number_arr [] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         char colour_arr [] = {'red_heart', 'red_karo', 'black_pik', 'black_cross'};
 
-}
+    public:
+        int number;
+        char colour;
 
+};
+
+
+void set_card(void){
+    number = number_arr[rand() % 10];
+    colour = colour_arr[rand() % 10];
+}
 
 int main() {
 
     Card c;
-    c.set_values();
+    c.set_card();
     cout << "Hello, World!" << endl;
     return 0;
 }
